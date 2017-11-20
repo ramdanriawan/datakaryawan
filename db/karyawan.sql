d7d95329808f5ff3ee112835b1cdc52d06d2a0d0
unknown <ramdanraiwan>
2017-11-20 19:50:25 +0700

    revisi selesai 2

--- db/karyawan.sql
+++ db/karyawan.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2017 at 04:48 PM
+-- Generation Time: Nov 20, 2017 at 01:49 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -212,6 +212,13 @@ CREATE TABLE `tbl_keluarga` (
   `kode_pos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tbl_keluarga`
+--
+
+INSERT INTO `tbl_keluarga` (`id_data_keluarga`, `username`, `nama`, `nip`, `status_perkawinan`, `foto_ktp_suami_istri`, `foto_kartu_keluarga`, `nama_suami_istri`, `nik_suami_istri`, `jumlah_anak`, `nama_anak_pertama`, `nik_anak_pertama`, `nama_anak_kedua`, `nik_anak_kedua`, `nama_anak_ketiga`, `nik_anak_ketiga`, `nama_ayah_kandung`, `nik_ayah_kandung`, `nama_ibu_kandung`, `nik_ibu_kandung`, `pekerjaan_suami_istri`, `nip_suami_istri`, `penghasilan_suami_istri`, `alamat_rumah`, `desa_kelurahan`, `kecamatan`, `kabupaten`, `provinsi`, `kode_pos`) VALUES
+(3, 'ulala uwaw mau', 'sdfsdfs', 'ASFSF', 'Janda', 'http://localhost/project/datak', 'http://localhost/project/datakaryawan/foto_kartu_keluarga/records.png', '4324324', '324', '32432', '4324', '432432', '432', '4324', '234324', '32432', '432432', '432432', '432432', '34324', '432432', '4324324', 'Rp.4.324.324,-', '4324324', '432432', '4324234', '4324324', '4324324', 234324);
+
 -- --------------------------------------------------------
 
 --
@@ -232,7 +239,9 @@ CREATE TABLE `tbl_login` (
 
 INSERT INTO `tbl_login` (`id`, `nama`, `username`, `password`, `session`) VALUES
 (1, 'admin', 'admin', 'admin', 'admin'),
-(2, 'user', 'user', 'user', 'user');
+(2, 'user', 'user', 'user', 'user'),
+(5, 'ramdan 6', 'Mohammad 3', '212', 'user3'),
+(10, 'sdfsdfs', 'ulala uwaw mau', '123', '');
 
 -- --------------------------------------------------------
 
@@ -302,6 +311,13 @@ CREATE TABLE `tbl_pegawai` (
   `no_hp` int(13) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tbl_pegawai`
+--
+
+INSERT INTO `tbl_pegawai` (`id_formulir_pegawai`, `username`, `nama`, `nip`, `pangkat_golongan`, `tmt_golongan`, `sk_pengangkatan_awal`, `tmt_pengangkatan_awal`, `mengampu_mata_pelajaran`, `jumlah_jam_mengajar`, `pejabat_yang_mengangkat`, `no_sk_cpns`, `tmt_sk_cpns`, `no_sk_pns`, `tmt_sk_pns`, `no_sk_pangkat_terakhir`, `tmt_sk_pangkat_terakhir`, `no_nuptk`, `no_karpeg`, `jabatan`, `unit_kerja`, `sk_pengangkatan_jabatan_fungsional_tertentu`, `dokumen_sertifikat_diklat`, `tmt_jabatan`, `masa_kerja_terakhir`, `masa_kerja_pengangkatan`, `tahun_pensiun`, `gaji_terakhir`, `sertifikasi`, `no_sertifikasi`, `tahun_sertifikasi`, `berkala_terakhir`, `no_berkala_terakhir`, `tanggal_berkala_berikutnya`, `no_berkala_berikutnya`, `no_hp`) VALUES
+(5, 'ulala uwaw mau', 'sdfsdfs', 0, 'Pengatur Muda, II/a', '11-November-2017', 'sfsdf', '22-November-2017', 'sdfsdf', '24', 'asdsf', 'adasd', '16-November-2017', 'adasd', '17-November-2017', 'sdfsdf', '23-November-2017', 'fdsfds', 'fdsfds', 'Guru Kelas', 'jlkjl', 'Kepala Sekolah', 'http://localhost/project/datakaryawan/dokumen_sertifikat_diklat/records.png', '25-November-2017', '8 Tahun 1 Bulan', '15-May-2020', '2077', 'Rp.0,-', 'Belum Sertifikasi', 'fdsfdsf', 'dsfdsf', 'dsfdsf', 'dsfdsf', 'fdsfds', 'fdsfd', 234324);
+
 -- --------------------------------------------------------
 
 --
@@ -330,6 +346,13 @@ CREATE TABLE `tbl_pendidikan` (
   `no_ijazah_sma` varchar(50) NOT NULL,
   `foto_ijazah_sma` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_pendidikan`
+--
+
+INSERT INTO `tbl_pendidikan` (`id_data_pendidikan`, `username`, `nama`, `nip`, `pendidikan_terakhir`, `no_ijazah_terakhir`, `foto_ijazah_terakhir`, `universitas`, `fakultas`, `jurusan`, `tahun_lulus`, `riwayat_pendidikan_sd`, `no_ijazah_sd`, `foto_ijazah_sd`, `riwayat_pendidikan_smp`, `no_ijazah_smp`, `foto_ijazah_smp`, `riwayat_pendidikan_sma`, `no_ijazah_sma`, `foto_ijazah_sma`) VALUES
+(3, 'ulala uwaw mau', 'sdfsdfs', 'ASFSF', 'DIII', 'fdfged', 'http://localhost/project/datakaryawan/foto_ijazah_terakhir/records.png', 'sfsdf', 'sdfsd', 'fsdfdsf', '423432', '234324', '432423', 'http://localhost/project/datakaryawan/foto_ijazah_sd/records.png', '324324', '432432', 'http://localhost/project/datakaryawan/foto_ijazah_smp/records.png', '4324', '324324', 'http://localhost/project/datakaryawan/foto_ijazah_sma/records.png');
 
 -- --------------------------------------------------------
 
@@ -375,6 +398,13 @@ CREATE TABLE `tbl_profile_pegawai` (
   `status_pegawai` varchar(50) NOT NULL,
   `jenjang_tempat_tugas` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_profile_pegawai`
+--
+
+INSERT INTO `tbl_profile_pegawai` (`id`, `kode`, `username`, `password`, `nama`, `nip`, `tempat_lahir`, `tanggal_lahir`, `nik`, `npwp`, `jenis_kelamin`, `status_pegawai`, `jenjang_tempat_tugas`) VALUES
+(6, '', 'ulala uwaw mau', '123', 'sdfsdfs', 'ASFSF', 'SDFSF', '11-November-2017', 42342, 0, 'Laki - Lak', 'PNS', 'PAUD');
 
 -- --------------------------------------------------------
 
@@ -452,6 +482,13 @@ CREATE TABLE `tbl_sekolah` (
   `nip_kepala_dinas` int(11) NOT NULL,
   `keterangan_lain` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_sekolah`
+--
+
+INSERT INTO `tbl_sekolah` (`id_data_sekolah`, `username`, `nama`, `nip`, `unit_kerja`, `jenjang_sekolah`, `nama_sekolah`, `uppk`, `nama_kepala_sekolah`, `nip_nama_kepala_sekolah`, `tanggal_lahir_kepala_sekolah`, `tempat_lahir_kepala_sekolah`, `jenis_kelamin_kepala_sekolah`, `pangkat_kepala_sekolah`, `pendidikan_terakhir_kepala_sekolah`, `tmt_kepala_sekolah_periode_pertama`, `tmt_kepala_sekolah_periode_kedua`, `tmt_kepala_sekolah_periode_ketiga`, `no_sk_kepala_sekolah_pertama`, `no_sk_kepala_sekolah_kedua`, `no_sk_kepala_sekolah_ketiga`, `no_sertifikat_nuk`, `foto_dokumen_nuk`, `akreditasi_sekolah`, `nss`, `npsn`, `status_sekolah`, `foto_sekolah`, `foto_dokumen_kegiatan_sekolah`, `jumlah_rombel`, `jumlah_tenaga_pengajar`, `jumlah_siswa_laki_laki`, `jumlah_siswa_perempuan`, `jumlah_siswa_keseluruhan`, `kebutuhan_guru`, `guru_yang_tersedia`, `kekurangan_guru`, `kelebihan_guru`, `ruang_keterampilan`, `lab_bahasa`, `lab_komputer`, `lab_ipa`, `perpustakaan`, `ruang_kelas`, `kondisi_ruangan`, `alat_pembelajaran`, `jumlah_guru_matematika`, `jumlah_guru_ipa`, `jumlah_guru_ips`, `jumlah_guru_sbk`, `jumlah_guru_bahasa_indonesia`, `jumlah_guru_bahasa_inggris`, `jumlah_guru_pjok`, `jumlah_guru_pkn`, `jumlah_guru_prakarya`, `jumlah_guru_agama_islam`, `jumlah_guru_agama_katolik`, `jumlah_guru_agama_protestan`, `jumlah_guru_agama_budha`, `jumlah_guru_agama_hindu`, `jumlah_pns`, `jumlah_gty`, `jumlah_gtk`, `jumlah_gtt`, `jumlah_gdd`, `kepala_dinas_sekarang`, `nip_kepala_dinas`, `keterangan_lain`) VALUES
+(1, 'ulala uwaw mau', 'sdfsdfs', 'ASFSF', 'jlkjl', 'TK', 'lilkj', 'Sandai', 'lkujlk', '324324', '24-November-2017', 'se4324', 'Perempuan', 'Pengatur Muda Tk.1, II/b', 'DIV', 'sdfsf', 'sdfsdf', 'sdf', 'sdfsd', 'fsdf', 'dsfsdf', '432432', 'http://localhost/project/datakaryawan/foto_dokumen_nuk/records.png', 'B', 0, 0, 'Negeri', 'http://localhost/project/datakaryawan/foto_sekolah/records.png', 'http://localhost/project/datakaryawan/foto_dokumen_kegiatan_sekolah/records.png', 324324, 4324324, 43243, 4324324, 32423, 4324324, 4324, 2344, 432432, 'Tidak Ada', 'Ada', 'Ada', 'Ada', 'Tidak Ada', 'Ada', 'Ada', 'Tidak Ada', 324, 2344, 2344, 432324, 3244, 432432, 432432, 43242, 3244, 343242, 42343, 324, 32432, 432432, 432432, 432423, 432432, 4324, 324324, '324324', 43243, '24324');
 
 -- --------------------------------------------------------
 
@@ -663,7 +700,8 @@ ALTER TABLE `tbl_jenjang_tempat_tugas`
 -- Indexes for table `tbl_keluarga`
 --
 ALTER TABLE `tbl_keluarga`
-  ADD PRIMARY KEY (`id_data_keluarga`);
+  ADD PRIMARY KEY (`id_data_keluarga`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `tbl_login`
@@ -682,13 +720,15 @@ ALTER TABLE `tbl_pangkat_golongan`
 -- Indexes for table `tbl_pegawai`
 --
 ALTER TABLE `tbl_pegawai`
-  ADD PRIMARY KEY (`id_formulir_pegawai`);
+  ADD PRIMARY KEY (`id_formulir_pegawai`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `tbl_pendidikan`
 --
 ALTER TABLE `tbl_pendidikan`
-  ADD PRIMARY KEY (`id_data_pendidikan`);
+  ADD PRIMARY KEY (`id_data_pendidikan`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `tbl_pendidikan_terakhir`
@@ -700,13 +740,15 @@ ALTER TABLE `tbl_pendidikan_terakhir`
 -- Indexes for table `tbl_profile_pegawai`
 --
 ALTER TABLE `tbl_profile_pegawai`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `tbl_sekolah`
 --
 ALTER TABLE `tbl_sekolah`
-  ADD PRIMARY KEY (`id_data_sekolah`);
+  ADD PRIMARY KEY (`id_data_sekolah`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `tbl_sertifikasi`
@@ -798,12 +840,12 @@ ALTER TABLE `tbl_jenjang_tempat_tugas`
 -- AUTO_INCREMENT for table `tbl_keluarga`
 --
 ALTER TABLE `tbl_keluarga`
-  MODIFY `id_data_keluarga` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_data_keluarga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `tbl_login`
 --
 ALTER TABLE `tbl_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `tbl_pangkat_golongan`
 --
@@ -813,12 +855,12 @@ ALTER TABLE `tbl_pangkat_golongan`
 -- AUTO_INCREMENT for table `tbl_pegawai`
 --
 ALTER TABLE `tbl_pegawai`
-  MODIFY `id_formulir_pegawai` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_formulir_pegawai` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `tbl_pendidikan`
 --
 ALTER TABLE `tbl_pendidikan`
-  MODIFY `id_data_pendidikan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_data_pendidikan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `tbl_pendidikan_terakhir`
 --
@@ -828,12 +870,12 @@ ALTER TABLE `tbl_pendidikan_terakhir`
 -- AUTO_INCREMENT for table `tbl_profile_pegawai`
 --
 ALTER TABLE `tbl_profile_pegawai`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `tbl_sekolah`
 --
 ALTER TABLE `tbl_sekolah`
-  MODIFY `id_data_sekolah` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_data_sekolah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `tbl_sertifikasi`
 --
